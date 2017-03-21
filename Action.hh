4967fbd929d4a1994c7a2f11e00ea910d1647f7d
wonderland64 <rasta-man92@hotmail.com>
2017-03-21 18:45:59 +0100

    the object is now a pointer

--- Action.hh
+++ Action.hh
@@ -6,30 +6,39 @@
 class IAction {
 public:
     virtual ~IAction() = default;
-    virtual void tickDown() = 0;
+    virtual bool tickDown() = 0;
 };
 
 
 // If R is not specified R defaults to void, result: if R is not specified F have to return void,
 // if R is specified F have to return R. Seems to work.
+// tickDown() returns true if the action is still valid, and false otherwise.
+
+// TODO: should probably refine the boolean bit.
 template<class T, class F, class R = void>
 class Action : public IAction{
 public:
-    Action(T ticks, F callback, R objectToModify = nullptr)
-        : ticks_(ticks), objectToModify_(objectToModify), callback_(callback) {}
+    Action(T ticks, F callback, R* objectToModify = nullptr)
+        : ticks_(ticks), objectToModify_(objectToModify), callback_(callback), invoked_(false) {}
 
-    void tickDown() override {
+    bool tickDown() override {
+        if (invoked_) {
+            //throw std::domain_error("already invoked, shouldn't have been called");
+            return false;
+        }
         if (ticks_ == 0) {
-            objectToModify_ = callback_();
+            *objectToModify_ = callback_();
+            invoked_ = true;
         }
         --ticks_;
-        return;
+        return !invoked_;
     }
 
 private:
     T ticks_;
-    R objectToModify_; 
+    R* objectToModify_; 
     F callback_;
+    bool invoked_;
 };
 
 #endif
