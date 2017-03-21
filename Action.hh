cfc743a7c7639c6916ecaf2cb0bc3a521e399b7c
wonderland64 <rasta-man92@hotmail.com>
2017-03-21 17:14:29 +0100

    added: Action takes an object that callback() assigns to

--- Action.hh
+++ Action.hh
@@ -9,14 +9,18 @@ public:
     virtual void tickDown() = 0;
 };
 
-template<class T, class F>
+
+// If R is not specified R defaults to void, result: if R is not specified F have to return void,
+// if R is specified F have to return R. Seems to work.
+template<class T, class F, class R = void>
 class Action : public IAction{
 public:
-    Action(T ticks, F callback) : ticks_(ticks), callback_(callback) {}
+    Action(T ticks, F callback, R objectToModify = nullptr)
+        : ticks_(ticks), objectToModify_(objectToModify), callback_(callback) {}
 
     void tickDown() override {
         if (ticks_ == 0) {
-            callback_();
+            objectToModify_ = callback_();
         }
         --ticks_;
         return;
@@ -24,6 +28,7 @@ public:
 
 private:
     T ticks_;
+    R objectToModify_; 
     F callback_;
 };
 
